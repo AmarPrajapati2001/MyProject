ec2a9bf26ff8b0ea6334925a901a2796d42fd85a
AmarPrajapati2001 <amarprajapati2010@gmail.com>
2024-07-12 00:31:39 +0530

    feat: add notes

---
+++ src/notes.tsx
@@ -0,0 +1,27 @@
+cd C:\Users\DELL\AppData\Local\Android\Sdk\platform-tools 
+
+
+------------------------------------------
+
+first run android studio project
+
+vs code :-  npx react-native init project --version 0.68.2  
+ 
+then 
+
+npx react-native init AwesomeProject  
+
+cd C:\Users\HARDIK\AppData\Local\Android\Sdk\platform-tools
+npx react-native run-android  open -a /Applications/Android\ Studio.app Adb reverse tcp:8081 tcp:8081   npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/resnvm alias default 20
+
+
+-----------------------------------------------------------------------
+npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+
+npm install @react-navigation/native @react-navigation/native-stack
+
+
+git push -u origin main
+React-Native/React/Redux snippets for es6/es7
+v2.0.6
